f55de43a212918301c52675cf88a7ae97b0b1e29
Mr. Senko <atodorov@mrsenko.com>
2016-08-14 22:56:35 +0300

    Add the removed-in directive as well

--- sphinx_removed_in/__init__.py
+++ sphinx_removed_in/__init__.py
@@ -4,12 +4,9 @@ from sphinx.directives.other import VersionChange
 
 
 def setup(app):
-    _directive = 'versionremoved'
-
-    if _directive in versionlabels:
-        return {'version': __version__}
-
-    versionlabels[_directive] = 'Removed in version %s'
-    app.add_directive(_directive, VersionChange)
+    for _directive in ['versionremoved', 'removed-in']:
+        if _directive not in versionlabels:
+            versionlabels[_directive] = 'Removed in version %s'
+            app.add_directive(_directive, VersionChange)
 
     return {'version': __version__}
