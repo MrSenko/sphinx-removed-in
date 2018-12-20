e940242e07de3e42addadf56b538fda92c21c5d6
Bruno Oliveira <nicoddemus@gmail.com>
2018-12-20 11:24:34 -0200

    Do not import sphinx at top level of sphinx_removed_in
    Follow up to https://github.com/MrSenko/sphinx-removed-in/pull/1: unfortunately `setup_requires` is not enough to fix the original issue, as `setup_requires` can't be evaluated yet (ref: https://github.com/pytest-dev/pytest/pull/4570#issuecomment-448993809).

--- sphinx_removed_in/__init__.py
+++ sphinx_removed_in/__init__.py
@@ -1,11 +1,10 @@
-from sphinx.locale import versionlabels
-from sphinx.directives.other import VersionChange
-
-
 __version__ = '0.1.1'
 
 
 def setup(app):
+    from sphinx.locale import versionlabels
+    from sphinx.directives.other import VersionChange
+    
     for _directive in ['versionremoved', 'removed-in']:
         if _directive not in versionlabels:
             versionlabels[_directive] = 'Removed in version %s'
