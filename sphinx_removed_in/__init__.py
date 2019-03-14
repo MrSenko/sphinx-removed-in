b19243952f9a672b533d609d01d7919a9b370a05
Miro Hrončok <miro@hroncok.cz>
2019-03-14 12:01:48 +0100

    Add support for Sphinx 2
    Fixes https://github.com/MrSenko/sphinx-removed-in/issues/5

--- sphinx_removed_in/__init__.py
+++ sphinx_removed_in/__init__.py
@@ -5,9 +5,15 @@ def setup(app):
     from sphinx.domains.changeset import versionlabels
     from sphinx.directives.other import VersionChange
 
+    try:
+        from sphinx.domains.changeset import versionlabel_classes
+    except ImportError:
+        versionlabel_classes = {}  # dummy dict for Sphinx < 2
+
     for _directive in ['versionremoved', 'removed-in']:
         if _directive not in versionlabels:
             versionlabels[_directive] = 'Removed in version %s'
+            versionlabel_classes[_directive] = 'removed'
             app.add_directive(_directive, VersionChange)
 
     return {'version': __version__}
