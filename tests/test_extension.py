0bedf2b999cf5284da92594126ca1ba4e47841bb
Miro Hrončok <miro@hroncok.cz>
2022-02-17 11:22:18 +0100

    Restore Python 2 compatibility

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -10,7 +10,12 @@ sys.path.insert(0, PARENT)
 @pytest.mark.sphinx(buildername='html', srcdir=os.path.join(PARENT, 'docs'))
 def test_sphinx_build(app, status, warning):
     app.build()
-    html = (app.outdir / 'index.html').read_text()
+    try:
+        html = (app.outdir / 'index.html').read_text()
+    except AttributeError:
+        # an older version of sphinx (used e.g. on Python 2)
+        # use the now deprecated API instead
+        html = (app.outdir / 'index.html').text()
 
     assert 'Removed in version 1.2' in html
     assert 'Removed in version 3.2' in html
